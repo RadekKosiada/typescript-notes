ee4f7cc9b5570cdc8e3b1c654de296062cd4a591
RadekKosiada <kosiadar@gmail.com>
2019-10-29 21:47:56 +0100

    Define boolean

--- types.js
+++ types.js
@@ -1,7 +1,8 @@
-"use strict";
-// exports.__esModule = true;
+//export{}; //https://stackoverflow.com/questions/35758584/cannot-redeclare-block-scoped-variable-typescript
 var myString;
 var myNumber;
+var myBoolean;
 myString = "Hello world!!!";
 myNumber = 22;
-console.log(myNumber);
+myBoolean = true;
+console.log(myBoolean);
--- types.ts
+++ types.ts
@@ -1,9 +1,11 @@
-export{}; //https://stackoverflow.com/questions/35758584/cannot-redeclare-block-scoped-variable-typescript
+//export{}; //https://stackoverflow.com/questions/35758584/cannot-redeclare-block-scoped-variable-typescript
 
 let myString: string;
 let myNumber: number;
+let myBoolean: boolean;
 
 myString = "Hello world!!!";
 myNumber = 22;
+myBoolean = true;
 
-console.log(myNumber);
+console.log(myBoolean);
