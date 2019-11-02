66a873c88159cfd2152a288fb22f4ca4155b7c10
RadekKosiada <kosiadar@gmail.com>
2019-11-02 20:01:29 +0100

    Add first function with defined types as arguments

---
+++ functions.js
@@ -0,0 +1,4 @@
+function getSum(num1, num2) {
+    return num1 + num2;
+}
+console.log(getSum(1, 4));
---
+++ functions.ts
@@ -0,0 +1,5 @@
+function getSum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+console.log(getSum(1, 4));
