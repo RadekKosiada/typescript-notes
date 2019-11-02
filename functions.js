cb2e31d435bc5023ba61b64079249004bd526ade
RadekKosiada <kosiadar@gmail.com>
2019-11-02 20:42:49 +0100

    Add question reg. optional argument

--- functions.js
+++ functions.js
@@ -12,6 +12,7 @@ var mySum = function (num1, num2) {
     return num1 + num2;
 };
 // console.log(mySum('3', 5))
+//why the optional argument is beneficial?
 function getName(firstName, lastName) {
     if (!lastName) {
         return firstName;
--- functions.ts
+++ functions.ts
@@ -16,11 +16,13 @@ let mySum = function (num1: any, num2: any): number {
 
 // console.log(mySum('3', 5))
 
+//why the optional argument is beneficial?
 function getName(firstName: string, lastName?: string): string {
   if(!lastName) {
     return firstName;
   }
   return firstName + ' ' + lastName;
 }
+
 
 console.log(getName('John'));
