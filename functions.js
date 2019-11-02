7e353c0c2ba84e64c885423957fe07630450bb00
RadekKosiada <kosiadar@gmail.com>
2019-11-02 20:06:49 +0100

    Add function w/parseInt

--- functions.js
+++ functions.js
@@ -1,4 +1,14 @@
 function getSum(num1, num2) {
     return num1 + num2;
 }
-console.log(getSum(1, 4));
+// console.log(getSum(1, 4));
+var mySum = function (num1, num2) {
+    if (typeof num1 == 'string') {
+        num1 = parseInt(num1);
+    }
+    if (typeof num2 == 'string') {
+        num2 = parseInt(num2);
+    }
+    return num1 + num2;
+};
+// console.log(mySum('3', 5))
--- functions.ts
+++ functions.ts
@@ -2,4 +2,18 @@ function getSum(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-console.log(getSum(1, 4));
+// console.log(getSum(1, 4));
+
+let mySum = function (num1: any, num2: any): number {
+  if(typeof num1 == 'string') {
+    num1 = parseInt(num1)
+  }
+  if(typeof num2 == 'string') {
+    num2 = parseInt(num2)
+  }
+  return num1 + num2;
+}
+
+// console.log(mySum('3', 5))
+
+
