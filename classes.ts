37846da0b55c68f9ef907461ecd6634c99b2efd8
RadekKosiada <kosiadar@gmail.com>
2019-11-03 18:30:50 +0100

    Add example of inheritance

--- classes.ts
+++ classes.ts
@@ -14,6 +14,9 @@ class User {
   register() {
     console.log(this.name + ' in now registered');
   }
+  payInvoice() {
+    console.log(this.name + ' payed invoice.')
+  }
 }
 
 let john = new User('John Doe', 'joe@mail.com', 34);
@@ -21,3 +24,18 @@ let john = new User('John Doe', 'joe@mail.com', 34);
 // console.log(john.age); //now will log error as age is a private property
 
 john.register();
+
+class Member extends User{
+  id: number;
+  constructor(id: number, name: string, email: string, age: number) {
+    super(name, email, age);
+    this.id = id;
+  }
+
+  payInvoice() {
+    super.payInvoice();
+  }
+}
+
+let mike: User = new Member(1, 'Mike Smith', 'mike@gmail.com', 34);
+mike.payInvoice();
