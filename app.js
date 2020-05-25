171d25b7a0722c1a003ce6cb0db36faeb7d7e040
r.kosiada <r.kosiada@hirschtec.eu>
2020-05-25 15:23:14 +0200

    Add boolean and string to ts file

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-function add(n1, n2) {
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        //if we add string and numbers within console.log it will print string
+        console.log(phrase + result);
+    }
     return n1 + n2;
 }
-var number1 = '5';
+var number1 = 5;
 var number2 = 3.4;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhase = 'Result is: ';
+add(number1, number2, printResult, resultPhase);
--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    const result = n1 + n2;
+    if (showResult) {
+        //if we add string and numbers within console.log it will print string
+        console.log(phrase + result);
+    }
     return n1 + n2;
 }
 
 const number1 = 5;
 const number2 = 3.4;
+const printResult = true;
+const resultPhase = 'Result is: '
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhase);
