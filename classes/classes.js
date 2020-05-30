3fc78e8865fd87e5fd3fef80e08453448929b3ce
r.kosiada <r.kosiada@hirschtec.eu>
2020-05-30 14:56:39 +0200

    Use 'this' within the class

--- classes/classes.js
+++ classes/classes.js
@@ -1,11 +1,19 @@
 "use strict";
 /* Declaring class */
-var Department = (function () {
+var Department = /** @class */ (function () {
     //constructor method
     function Department(n) {
         this.name = n;
     }
+    /* adding method */
+    /* using 'this' keyword */
+    Department.prototype.describe = function () {
+        console.log('Department: ' + this.name);
+    };
     return Department;
 }());
 var accounting = new Department('Accounting');
 console.log(accounting);
+accounting.describe();
+var accountingCopy = { name: 's', describe: accounting.describe };
+accountingCopy.describe();
--- classes/classes.ts
+++ classes/classes.ts
@@ -6,7 +6,18 @@ class Department {
     constructor(n: string) {
         this.name = n;
     }
+    /* adding method */
+    /* using 'this' keyword */
+    describe(this: Department) {
+        console.log('Department: ' + this.name)
+    }
 }
 
 const accounting = new Department('Accounting');
 console.log(accounting)
+
+accounting.describe();
+
+const accountingCopy = { name: 's', describe: accounting.describe };
+
+accountingCopy.describe();
