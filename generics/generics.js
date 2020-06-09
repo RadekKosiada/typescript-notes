6574509b69ac2c498a314357424eabf8485cea0d
r.kosiada <r.kosiada@hirschtec.eu>
2020-06-09 17:23:29 +0200

    Introduce Generics

--- generics/generics.js
+++ generics/generics.js
@@ -1,1 +1,6 @@
 "use strict";
+/** 3 ways of defining a string */
+var arr = ['string1', 'string2'];
+var arr1 = [];
+/** a way to define an **generic** array type */
+var arr2 = [];
---
+++ generics/generics.ts
@@ -0,0 +1,10 @@
+/** 3 ways of defining a string */
+const arr = ['string1', 'string2'];
+
+const arr1: string[] = [];
+
+/** a way to define an **generic** array type */
+const arr2: Array<string> = [];
+
+
+
