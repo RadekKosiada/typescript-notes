3a67fc16eb8972d4c51251617e50b05d51d0e5fc
r.kosiada <r.kosiada@hirschtec.eu>
2020-06-09 17:29:24 +0200

    Promise as generic type

--- generics/generics.js
+++ generics/generics.js
@@ -4,3 +4,12 @@ var arr = ['string1', 'string2'];
 var arr1 = [];
 /** a way to define an **generic** array type */
 var arr2 = [];
+/** Promise as generic type
+ *
+ * gives information about what to expect from promise
+*/
+var promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve('This is done!');
+    }, 2000);
+});
--- generics/generics.ts
+++ generics/generics.ts
@@ -6,5 +6,14 @@ const arr1: string[] = [];
 /** a way to define an **generic** array type */
 const arr2: Array<string> = [];
 
+/** Promise as generic type 
+ * 
+ * gives information about what to expect from promise
+*/
 
+const promise: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('This is done!');
+    }, 2000);
+});
 
